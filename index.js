540793956e809f32094cdbc6aa3aa212b66571df
Mahfuz Alam <36205825+Mahfuz-mah@users.noreply.github.com>
2020-05-28 18:13:41 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+let resultField=$("#result");
+function insertNum(number){
+    let existingNumber= resultField.val();
+    resultField.val(existingNumber+number);
+}
+
+function clearResult(){
+    resultField.val('');
+
+}
+
+function calculate(){
+
+    let result=eval( resultField.val());
+    resultField.val(result);
+
+}
+
+function deletNumber(){
+    let presentValue=resultField.val();
+    if(presentValue!=''){
+        resultField.val(presentValue.slice(0,-1));
+    }
+
+}
